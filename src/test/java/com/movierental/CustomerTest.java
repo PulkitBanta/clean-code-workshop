205469f33865fe5523319900ef9e6a5086c7ec54
Pulkit Banta <pulkit.banta@medlypharmacy.com>
2021-04-26 21:14:22 +0530

    characterization test

--- src/test/java/com/movierental/CustomerTest.java
+++ src/test/java/com/movierental/CustomerTest.java
@@ -2,8 +2,22 @@ package com.movierental;
 
 import org.junit.Test;
 
-public class CustomerTest {
-    @Test
-    public void test(){}
+import static org.junit.Assert.assertEquals;
 
-}
+public class CustomerTest {
+
+    @Test
+    public void shouldCheckTheStatementForMultipleRentals() {
+        Customer customer = new Customer("Test");
+        customer.addRental(new Rental(new Movie("Abc", 2), 5));
+        customer.addRental(new Rental(new Movie("Def", 1), 3));
+        customer.addRental(new Rental(new Movie("Ghi", 0), 2));
+        assertEquals("Rental Record for Test\n" +
+                "\tAbc\t4.5\n" +
+                "\tDef\t9.0\n" +
+                "\tGhi\t2.0\n" +
+                "Amount owed is 15.5\n" +
+                "You earned 4 frequent renter points", customer.statement());
+    }
+
+} 
